b0ccad9979ff425d8e1b32a0ab332486ac83e71d
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-13 01:13:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,6 @@ def introduction(name)
   end
 end
 
-# lib/introduction.rb
 def introduction_with_(language) def introduction(name)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
   end
